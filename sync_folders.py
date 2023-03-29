be73be41c06efd158c3ab2e3a6d675312c8384cb
yunissalih <yunissalih.ys@gmail.com>
2023-03-29 21:19:53 +0300

    commented some lines

--- sync_folders.py
+++ sync_folders.py
@@ -17,17 +17,19 @@ def getTime():
 
 
 def sync(src, dest):
+
+    # traverse through the source folder
     for root, fldrs, files in os.walk(src):
         dest_path = root.replace(src, dest)
 
-        # Synchronize files
+        # copy files to replica
         for file in files:
             file_src = os.path.join(root, file)
             file_dest = os.path.join(dest_path, file)
             shutil.copy(file_src, file_dest)
             log(f"[[ {getTime()} ]] File {file} copied to {file_dest}.")
 
-        # Synchronize directories
+        # copy directories to replica
         for fldr in fldrs:
             try:
                 fldr_dest = os.path.join(dest_path, fldr)
@@ -36,8 +38,11 @@ def sync(src, dest):
             except OSError:
                 pass
 
+    # traverse through replica folder
     for root, fldrs, files in os.walk(dest):
         src_path = root.replace(dest, src)
+
+        # delete files no longer present in src folder
         for file in files:
             file_src = os.path.join(src_path, file)
             file_dest = os.path.join(root, file)
@@ -46,8 +51,7 @@ def sync(src, dest):
                 os.remove(file_dest)
                 log(f"[[ {getTime()} ]] File deleted: {file} from {file_dest}")
 
-
-        # Synchronize directories
+        # delete folders no longer present in src folder
         for fldr in fldrs:
             fldr_path = os.path.join(root, fldr)
             fldr_src = os.path.join(src_path, fldr)
